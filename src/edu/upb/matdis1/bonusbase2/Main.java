d14c7dd665f54ef24e5d12bd02b443c05bdadb23
pedro.gomez.2019 <pedro.gomez.2019@upb.edu.co>
2020-02-24 17:02:53 -0500

    Creating decimal to binary method

--- src/edu/upb/matdis1/bonusbase2/Main.java
+++ src/edu/upb/matdis1/bonusbase2/Main.java
@@ -1,10 +1,15 @@
 package edu.upb.matdis1.bonusbase2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+public int decimal_bin(int n){
+     List<Integer> module = new ArrayList<>();
+    return 0;
+ }
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Test!");
-        System.out.println("Pedro!");
+
     }
 }
